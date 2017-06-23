40ffd238b7416845f42d6caa5ee3257469914fef
Tohru Hashimoto <hasimo-t@any-door.com>
2017-06-23 10:15:46 +0900

    change the selenium port

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,7 @@ export DISPLAY=${DISPLAY:-:"10"}
 /usr/bin/Xvfb ${DISPLAY} -screen 0 1366x768x24 -ac +render -noreset & 
 sleep 10;
 x11vnc --display ${DISPLAY} -geometry 1366x768 --forever & 
-geckodriver & 
-SELENIUM_PORT=${SELENIUM_PORT:-"4444"}
-exec /usr/bin/java  -Dwebdriver.gecko.driver=/usr/bin -jar /opt/selenium-server.jar -port ${SELENIUM_PORT} 
+SELENIUM_PORT=${SELENIUM_PORT:-"5555"}
+exec /usr/bin/java  -Dwebdriver.gecko.driver=/usr/bin/geckodriver -jar /opt/selenium-server.jar -port ${SELENIUM_PORT} 
 
 
