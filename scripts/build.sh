bfb76053e5de9291cfcf3395c17d159aaea31962
Tohru Hashimoto <hasimo-t@any-door.com>
2018-02-20 11:54:31 +0900

    update to 3.9.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,18 @@
 set -ex
 apt-get update 
 
-apt-get install -y --no-install-recommends software-properties-common xvfb x11vnc  libasound2 libxcomposite1 x11vnc fonts-ipafont-gothic openjdk-9-jre-headless firefox 
+apt-get install -y --no-install-recommends software-properties-common xvfb x11vnc  libasound2 libxcomposite1 x11vnc fonts-ipafont-gothic openjdk-9-jre-headless firefox dbus
 
 apt-get clean 
 rm -rf /var/lib/apt/lists/* 
-curl http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-${SELENIUM_VERSION}.jar -o /opt/selenium-server.jar
+dbus-uuidgen > /etc/machine-id
+IFS_ORIGINAL="$IFS"
+IFS=.
+set -- $SELENIUM_VERSION
+SELENIUM_MINOR_VERSION="$1.$2"
+IFS="$IFS_ORIGINAL"
+
+curl http://selenium-release.storage.googleapis.com/$SELENIUM_MINOR_VERSION/selenium-server-standalone-${SELENIUM_VERSION}.jar -o /opt/selenium-server.jar
 
 # geckdriver 
 curl -L https://github.com/mozilla/geckodriver/releases/download/v${GECKODRIVER_VERSION}/geckodriver-v${GECKODRIVER_VERSION}-linux64.tar.gz -o /tmp/geckodriver.tar.gz
@@ -15,7 +22,7 @@ cd /tmp/
 tar xvzf geckodriver.tar.gz
 cp /tmp/geckodriver /usr/bin/
 
-## chrome driver
+### chrome driver
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 #echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 #apt-get install -y --no-install-recommends google-chrome-stable dbus
@@ -24,4 +31,4 @@ cp /tmp/geckodriver /usr/bin/
 #cd /tmp/
 #unzip chromedriver.zip
 #cp /tmp/chromedriver /usr/bin/
-
+#
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ ulimit -n 8192
 
 export DISPLAY=${DISPLAY:-:"10"}
 /usr/bin/Xvfb ${DISPLAY} -screen 0 1366x768x24 -ac +render -noreset & 
-sleep 10;
+sleep 10
 x11vnc --display ${DISPLAY} -geometry 1366x768 --forever & 
 SELENIUM_PORT=${SELENIUM_PORT:-"5555"}
 exec /usr/bin/java  -Dwebdriver.gecko.driver=/usr/bin/geckodriver -jar /opt/selenium-server.jar -port ${SELENIUM_PORT} 
